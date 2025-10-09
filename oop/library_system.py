ab2107a2f83b7cf5d5c5e95b74e18c9c408f8dc5
Kevin Njagi <njagikl@gmail.com>
2025-10-09 16:27:55 +0300

    Add __str__ to Book/EBook/PrintBook and use in Library.list_books

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,17 +5,26 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = int(file_size)
 
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = int(page_count)
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -27,9 +36,4 @@ class Library:
 
     def list_books(self):
         for b in self.books:
-            if isinstance(b, EBook):
-                print(f"EBook: {b.title} by {b.author}, File Size: {b.file_size}KB")
-            elif isinstance(b, PrintBook):
-                print(f"PrintBook: {b.title} by {b.author}, Page Count: {b.page_count}")
-            else:
-                print(f"Book: {b.title} by {b.author}")
+            print(str(b))
